5771c87b67b4319b84b6b67e872acc41eb3914da
tdeenes <toth.denes@ttk.mta.hu>
2015-07-07 16:44:07 +0200

    forgot to modify the argumentDeparser call in tanova()

--- R/anova.R
+++ R/anova.R
@@ -1275,9 +1275,8 @@ tanova <- function(.arraydat, factordef, bwdat = NULL,
                      .var.name = "nperm")
     type <- match.arg(type)
     mcall <- match.call() 
-    args <- as.list(mcall[-1])
     ob <- getDoBackend()
-    parallel <- argumentDeparser(args$parallel, "parallelParams")
+    parallel <- argumentDeparser(substitute(parallel), "parallelParams")
     if (is.logical(parallel) && !parallel) {
         registerDoSEQ()
     } else if (inherits(parallel, "parallelParams") && parallel$cl_new) {
